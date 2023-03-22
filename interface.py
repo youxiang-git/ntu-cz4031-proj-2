4803c29f81e41af067fac76069c153c099feb11b
Kasidis Chantharojwong <me@ckasidis.com>
2023-03-23 02:24:44 +0800

    style: add tailwindcss

--- interface.py
+++ interface.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__,
+            template_folder='templates')
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
-   app.run()
+    app.run()

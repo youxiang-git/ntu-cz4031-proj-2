91bf74b04105b97e17c1a4f52f7ce677db7fd5a0
Kasidis Chantharojwong <me@ckasidis.com>
2023-04-12 13:37:21 +0800

    feat: update GUI

--- interface.py
+++ interface.py
@@ -1,12 +1,3 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__,
             template_folder='templates')
-
-explanation = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi laboriosam, doloremque, aspernatur expedita quisquam asperiores voluptatibus magni odio voluptatem recusandae sed et aliquam, inventore ducimus? Quos quae nisi sunt ex!'
-
-@app.route('/')
-def home():
-    return render_template('index.html', explanation=explanation)
-
-if __name__ == '__main__':
-    app.run()
--- project.py
+++ project.py
@@ -1,10 +1,13 @@
-from explain import *
+from flask import render_template, jsonify, request
+from explain import CompareQueries
+from interface import app
 
-sample_query1 = "select c.c_name, o.o_orderkey, n.n_name from customer c INNER JOIN orders o ON c.c_custkey = o.o_custkey INNER JOIN nation n ON c.c_nationkey = n.n_nationkey WHERE c.c_name = 'Customer#000000316' LIMIT 100"
-sample_query2 = "select c.c_name, o.o_orderkey, n.n_name from customer c INNER JOIN orders o ON c.c_custkey = o.o_custkey INNER JOIN nation n ON c.c_nationkey = n.n_nationkey WHERE c.c_name = 'Customer#000000316';"
+# sample_query1 = "select c.c_name, o.o_orderkey, n.n_name from customer c INNER JOIN orders o ON c.c_custkey = o.o_custkey INNER JOIN nation n ON c.c_nationkey = n.n_nationkey WHERE c.c_name = 'Customer#000000316' LIMIT 100"
+# sample_query2 = "select c.c_name, o.o_orderkey, n.n_name from customer c INNER JOIN orders o ON c.c_custkey = o.o_custkey INNER JOIN nation n ON c.c_nationkey = n.n_nationkey WHERE c.c_name = 'Customer#000000316';"
 
 # sample_query1 = "select * from customer c, nation n where n.n_nationkey = c.c_nationkey order by c_nationkey asc"
 # sample_query2 = "select c.c_name from customer c, nation n where n.n_nationkey = c.c_nationkey order by c_nationkey asc"
+
 # sample_query1 = "select c_name, o_comment from customer inner join orders on c_custkey = o_orderkey;"
 # sample_query2 = "select c_name, o_comment from customer inner join orders on c_custkey = o_custkey;"
 
@@ -17,20 +20,40 @@ sample_query2 = "select c.c_name, o.o_orderkey, n.n_name from customer c INNER J
 # sample_query1 = "SELECT sum(C.c_acctbal) FROM customer AS c, nation AS n WHERE c.c_nationkey = n.n_nationkey AND n.n_name = 'INDIA'"
 # sample_query2 = "SELECT sum(C.c_acctbal) FROM customer AS c, nation AS n WHERE c.c_nationkey = n.n_nationkey AND n.n_name = 'ARGENTINA'"
 
-plan1diff, plan2diff, qdiff = CompareQueries(sample_query1, sample_query2)
+# plan1diff, plan2diff, qdiff = CompareQueries(sample_query1, sample_query2)
 
-print("P has the following extra operations:")
-for i in plan1diff:
-    print(i)
+# print("P has the following extra operations:")
+# for i in plan1diff:
+#     print(i)
 
-print()
+# print()
 
-print("P' has the following extra operations:")
-for i in plan2diff:
-    print(i)
+# print("P' has the following extra operations:")
+# for i in plan2diff:
+#     print(i)
 
-print()
+# print()
 
-print("The queries are different in the following ways:")
-for i in qdiff:
-    print(i)
+# print("The queries are different in the following ways:")
+# for i in qdiff:
+#     print(i)
+
+
+@ app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/compare', methods=['POST'])
+def compare():
+    data = request.get_json()
+
+    query1 = data['query1']
+    query2 = data['query2']
+    query1Plan, query2Plan, diff = CompareQueries(query1, query2)
+
+    return jsonify(query1Plan=query1Plan, query2Plan=query2Plan, diff=diff)
+
+
+if __name__ == '__main__':
+    app.run()
